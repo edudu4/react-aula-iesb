8eb5e6879342aaf0a0040e66fcb129ad14ac6660
Eduardo Alves <eduardoa8142@gmail.com>
2023-04-18 21:10:04 -0300

    Estruturando Appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,34 @@
 export default function App() {
   return (
-    <></>
-
+    <>
+    <header>
+      <h1>Meu Site React</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Meus Pedidos</a></li>
+          <li><a href="#">Novo Pedido</a></li>
+          <li><a href="#">Sobre</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <section>
+        <h2>Página Inicial</h2>
+        </section>
+      <section>
+        <h2>Meus Pedidos</h2>
+      </section>
+      <section>
+        <h2>Novo Pedido</h2>
+      </section>
+      <section>
+        <h2>Página Sobre</h2>
+      </section>
+    </main>
+    <footer>
+      <p>Direitos Autorais</p>
+    </footer>
+    </>
   )
 }
